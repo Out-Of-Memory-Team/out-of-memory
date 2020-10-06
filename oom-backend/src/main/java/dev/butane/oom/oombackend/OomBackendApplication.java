ca2288bc27eda290212136c732928528fc39e3bf
jgottfriedMSC <jgottfried@mainstorconcept.com>
2020-10-06 11:08:47 +0200

    Initial Commit - SetUp Technology and Project

---
+++ oom-backend/src/main/java/dev/butane/oom/oombackend/OomBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.butane.oom.oombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OomBackendApplication.class, args);
+	}
+
+}
---
+++ oom-backend/src/test/java/dev/butane/oom/oombackend/OomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.butane.oom.oombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
