84cc9dc0b8fd1ebbba80d283ca58f24ad98ebcd4
Kronnox <dev@kronox.me>
2020-12-04 11:01:58 +0100

    removes mock code

--- oom-backend/src/main/java/dev/butane/oom/oombackend/OomBackendApplication.java
+++ oom-backend/src/main/java/dev/butane/oom/oombackend/OomBackendApplication.java
@@ -19,30 +19,4 @@ public class OomBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OomBackendApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner init(FlashcardRepository flashcardRepository, DeckRepository deckRepository, TagRepository tagRepository, UserRepository userRepository) {
-		User u = new User("user", "justin", "gottfried", "justin@gottfried.com");
-		User u1 = new User("user2", "jay", "G", "jayg@g.com");
-
-		u = userRepository.save(u);
-		u1 = userRepository.save(u1);
-
-		Set<Tag> tags = new HashSet<>();
-		tags.add(new Tag("test"));
-		tags.add(new Tag("wololo"));
-
-		Deck d = new Deck("test", "testinhalt", Visibility.PUBLIC, u, tags);
-
-		d.getCollaborators().add(u1);
-
-		for (int i = 0; i < 10; i++) {
-			Flashcard f = new Flashcard("title " + i, "front " + i, "back " + i, d);
-			d.getFlashcards().add(f);
-		}
-
-		deckRepository.save(d);
-
-		return args -> {};
-	}
 }
