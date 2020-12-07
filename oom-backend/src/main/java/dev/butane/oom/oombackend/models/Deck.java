3fb53201927763b647e8ba55b4d9dace8f2c772c
Kronnox <dev@kronox.me>
2020-12-07 09:27:04 +0100

    minor db adjustments

--- oom-backend/src/main/java/dev/butane/oom/oombackend/models/Deck.java
+++ oom-backend/src/main/java/dev/butane/oom/oombackend/models/Deck.java
@@ -1,22 +1,17 @@
 package dev.butane.oom.oombackend.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.sun.istack.NotNull;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.*;
 
-@Getter
-@Setter
-@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "deck")
 public class Deck {
@@ -58,22 +53,4 @@ public class Deck {
         inverseJoinColumns = @JoinColumn(name = "name")
     )
     private Set<Tag> tags = new HashSet<Tag>();
-
-    public Deck(String title, String description, Visibility visibility, User maintainer, Set<Tag> tags) {
-        this.deckId = UUID.randomUUID();
-        this.title = title;
-        this.description = description;
-        this.visibility = visibility;
-        this.maintainer = maintainer;
-        this.tags = tags;
-    }
-
-    public Deck() {
-        this.title = "";
-        this.description = "";
-        this.visibility = Visibility.PRIVATE;
-        this.maintainer = new User();
-        this.collaborators = null;
-        this.flashcards = null;
-    }
 }
