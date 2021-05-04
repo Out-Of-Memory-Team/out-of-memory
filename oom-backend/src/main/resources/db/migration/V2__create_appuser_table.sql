ac7d0e28a3f4bbf6003433fbb285e34790fc82fc
jgottfriedMSC <58469498+jgottfriedMSC@users.noreply.github.com>
2021-05-04 12:36:10 +0200

    Merge conflicts (#12)
    * fixes fe imports
    
    * Fixed DB Mapping Issues etc.
    
    * mvn verify works!
    
    Co-authored-by: Kronnox <dev@kronox.me>

--- oom-backend/src/main/resources/db/migration/V2__create_appuser_table.sql
+++ oom-backend/src/main/resources/db/migration/V2__create_appuser_table.sql
@@ -4,6 +4,12 @@ CREATE TABLE IF NOT EXISTS oom.appuser
     firstname character varying(50) COLLATE pg_catalog."default",
     email text COLLATE pg_catalog."default" NOT NULL,
     lastname character varying(50) COLLATE pg_catalog."default",
+    password character varying(50) COLLATE pg_catalog."default",
+    accountNonExpired boolean,
+    accountNonLocked boolean,
+    credentialsNonExpired boolean,
+    enabled boolean,
+    role character varying(50) COLLATE pg_catalog."default" NOT NULL,
     userId uuid NOT NULL,
     CONSTRAINT "PK_User" PRIMARY KEY (userId)
 )
