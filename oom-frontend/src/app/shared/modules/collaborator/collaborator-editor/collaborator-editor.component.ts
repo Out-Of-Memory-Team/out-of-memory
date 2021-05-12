29f73bdd98ae3c5a9b8e55d91f8f9e4f4ef372ec
leonhardstengel <leonhard.stengel@outlook.de>
2021-05-12 16:15:42 +0200

    tidy up collaborator-component

--- oom-frontend/src/app/shared/modules/collaborator/collaborator-editor/collaborator-editor.component.ts
+++ oom-frontend/src/app/shared/modules/collaborator/collaborator-editor/collaborator-editor.component.ts
@@ -12,43 +12,27 @@ export class CollaboratorEditorComponent {
   @Input() collaborators: User[] = [];
 
   @Output() collaboratorsChange: EventEmitter<User[]> = new EventEmitter<User[]>();
-  @Output() collaboratorSelect: EventEmitter<User> = new EventEmitter<User>();
-  @Output() collaboratorDelete: EventEmitter<User> = new EventEmitter<User>();
-  @Output() collaboratorAdd: EventEmitter<User> = new EventEmitter<User>();
 
   selected: number = -1;
   openAdd: boolean = false;
-  newCollaboratorName: string;
 
   constructor(private usersBackend: UsersBackendService) { }
 
   select(index: number): void {
     this.selected = index;
-    this.collaboratorSelect.emit(this.collaborators[index]);
   }
 
   delete(index: number): void {
     this.collaborators.splice(index, 1);
     this.collaboratorsChange.emit(this.collaborators);
-    this.collaboratorDelete.emit(this.collaborators[index]);
   }
 
   add(collaborator: User): void {
     this.collaborators.push(collaborator);
     this.collaboratorsChange.emit(this.collaborators);
-    this.collaboratorAdd.emit(collaborator);
   }
 
   triggerAddModal(): void {
     this.openAdd = true;
-  }
-
-  submitAddModal(): void {
-    if(!this.collaborators.findIndex(t => t.username === this.newCollaboratorName))
-      return;
-
-    this.usersBackend.getUserByUsername(this.newCollaboratorName).subscribe(user => {
-      this.add(user);
-    })
   }
 }
