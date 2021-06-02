bdb44d7d3d98d57c89be041b25ca5839fe71348f
jgottfriedMSC <jgottfried@mainstorconcept.com>
2021-06-02 14:33:43 +0200

    Removed Testcontainers

--- oom-backend/src/test/java/dev/butane/oom/oombackend/repositories/UserRepositoryTests.java
+++ oom-backend/src/test/java/dev/butane/oom/oombackend/repositories/UserRepositoryTests.java
@@ -6,37 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
-@Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTests {
-
-    @Container
-    static PostgreSQLContainer database = new PostgreSQLContainer("postgres:12").withDatabaseName("oom")
-            .withPassword("password")
-            .withUsername("Justin_Gottfried@hotmail.com");
-
-    @DynamicPropertySource
-    static void setDataSourceProperties(DynamicPropertyRegistry propertyRegistry) {
-        propertyRegistry.add("spring.datasource.url", database::getJdbcUrl);
-        propertyRegistry.add("spring.datasource.password", database::getPassword);
-        propertyRegistry.add("spring.datasource.username", database::getUsername);
-    }
 
     @Autowired
     private UserRepository userRepository;
