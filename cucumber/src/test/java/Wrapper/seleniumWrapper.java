b4ac002a5fa7b31b0674a09642b6d02128ba5536
FranzJosef2000 <franz-josef.nitz@web.de>
2021-06-03 15:25:19 +0200

    clean up login feature wit Wrapper

--- cucumber/src/test/java/Wrapper/seleniumWrapper.java
+++ cucumber/src/test/java/Wrapper/seleniumWrapper.java
@@ -1,2 +1,35 @@
-package stepDefinitions;public class seleniumWrapper {
+package Wrapper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class seleniumWrapper {
+    WebDriver driver = null;
+    public WebElement findWebElementWithText(String text) {
+        return driver.findElement(By.xpath(".//*[contains(.,'$text')]"));
+    }
+    public WebElement findElementWithId(String id){
+        return driver.findElement(By.id(id));
+    }
+    public WebElement findWebButtonWithText(String text){
+        return driver.findElement(By.xpath(".//button[contains(.,'"+text+"')]"));
+    }
+    public WebElement findWebElementWithPlaceholderText(String text) {
+        return driver.findElement(By.xpath(".//*[@placeholder='"+text+"']"));
+    }
+
+    public void setDriver(){
+        driver = new ChromeDriver();
+    }
+    public void setTimeout(){
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+    }
+    public void navigateTo(String url){
+        driver.navigate().to(url);
+    }
 }
