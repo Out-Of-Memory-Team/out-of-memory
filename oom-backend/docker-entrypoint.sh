710ffa069f360628405036f4ff6fc81624ff6b65
Kronnox <dev@kronox.me>
2021-06-14 18:35:57 +0200

    fixes networking

--- oom-backend/docker-entrypoint.sh
+++ oom-backend/docker-entrypoint.sh
@@ -6,6 +6,7 @@ java \
 -Dspring.datasource.url=$DB_URL \
 -Dspring.datasource.username=$DB_USER \
 -Dspring.datasource.password=$DB_PASSWORD \
+-Dserver.port=$SERVER_PORT \
 -jar /app.jar
 
 exec "$@"
