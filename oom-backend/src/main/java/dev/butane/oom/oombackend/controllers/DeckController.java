425b01cc994aff1dea225dcf52506385b47638e9
leonhardstengel <leonhard.stengel@outlook.de>
2021-06-14 15:31:40 +0200

    Auth for Decks-API

--- oom-backend/src/main/java/dev/butane/oom/oombackend/controllers/DeckController.java
+++ oom-backend/src/main/java/dev/butane/oom/oombackend/controllers/DeckController.java
@@ -27,28 +27,30 @@ public class DeckController {
     // Get all decks
     @GetMapping("/decks")
     public Optional<Deck[]> getDecks(Principal principal){
-        return deckRepository.findAllByMaintainer_UserId(UUID.fromString(principal.getName()));
+        return deckRepository.findAllDecksByUser(getCurrentUser(principal));
     }
 
     // Get deck by id
     @GetMapping("/decks/{id}")
     public Optional<Deck> getDeck(@PathVariable UUID id, Principal principal) {
-        User user = userRepository.findById(UUID.fromString(principal.getName())).orElseThrow(RuntimeException::new);
-        return deckRepository.findByDeckIdAndMaintainerOrCollaboratorsContains(id, user, new User());
+        return deckRepository.findById(id);
     }
 
     // Create or Update a Deck
     @PutMapping("/decks")
     public Deck createOrUpdateDeck(@RequestBody Deck deck, Principal principal) {
-        //if(deck.getDeckId() == UUID.fromString(principal.getName()))
+        if(deck.getMaintainer() == this.getCurrentUser(principal) || deck.getCollaborators().contains(this.getCurrentUser(principal)))
             return deckRepository.save(deck);
-        //throw new RuntimeException();
+        throw new RuntimeException();
     }
 
     // Delete a Deck
     @DeleteMapping("/decks/{id}")
     public void deleteDeck(@PathVariable UUID id, Principal principal) {
-        User user = userRepository.findById(UUID.fromString(principal.getName())).orElseThrow(RuntimeException::new);
-        deckRepository.deleteByDeckIdAndMaintainer(id, user);
+        deckRepository.deleteById(id);
+    }
+
+    private User getCurrentUser(Principal principal) {
+        return userRepository.findById(UUID.fromString(principal.getName())).orElseThrow(RuntimeException::new);
     }
 }
--- oom-backend/src/main/java/dev/butane/oom/oombackend/repositories/DeckRepository.java
+++ oom-backend/src/main/java/dev/butane/oom/oombackend/repositories/DeckRepository.java
@@ -2,6 +2,7 @@ package dev.butane.oom.oombackend.repositories;
 
 import dev.butane.oom.oombackend.models.Deck;
 import dev.butane.oom.oombackend.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,9 @@ import java.util.UUID;
 
 @Repository
 public interface DeckRepository extends CrudRepository<Deck, UUID> {
-    Optional<Deck[]> findAllByMaintainer_UserId(UUID maint);
-    Optional<Deck> findByDeckIdAndMaintainerOrCollaboratorsContains(UUID id, User maint, User collab);
-    Optional<Deck> deleteByDeckIdAndMaintainer(UUID id, User user);
+    @Query(
+            "SELECT d FROM Deck d WHERE d.maintainer = ?1"
+            + "OR ?1 MEMBER OF d.collaborators"
+    )
+    Optional<Deck[]> findAllDecksByUser(User user);
 }
